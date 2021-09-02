51d7cec49f9f3d2f792a1b3ccb534dc36e3fedcf
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-09-02 08:44:23 +0200

    Add files via upload

---
+++ Java/7kyu/String ends with ¿.java
@@ -0,0 +1,12 @@
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    
+    if(str.endsWith(ending)){
+      return true;
+    
+    } else {
+      return false;
+    }
+    
+  }
+}
