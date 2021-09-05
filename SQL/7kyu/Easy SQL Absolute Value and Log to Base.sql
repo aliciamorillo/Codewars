c911fa8339148d156d906daa2f24958d7734bc19
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-09-05 20:50:06 +0200

    Add files via upload

---
+++ SQL/7kyu/Easy SQL Absolute Value and Log to Base.sql
@@ -0,0 +1,2 @@
+SELECT abs(number1) AS abs, log(64, number2) AS log
+FROM decimals;
---
+++ SQL/8kyu/Register for the Party (SQL for Beginners #3).sql
@@ -0,0 +1,5 @@
+INSERT INTO participants(name, age, attending)
+
+VALUES ('name', 21, true);
+
+SELECT * FROM participants;
